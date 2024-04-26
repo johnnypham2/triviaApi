eac8c55acb1a06934cf8991c51c7bbd1b97574d9
Johnnypham2 <phammjohnny@gmail.com>
2024-04-25 20:42:52 -0700

    added my category

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,24 @@ let questionElement = document.getElementById("question");
 let answerElement = document.getElementById("answer");
 let questionButton = document.getElementById("btn");
 let answerButton = document.getElementById("btn2");
+const categorySelect = document.getElementById("category");
+
 
 const getQuestion = async () => {
-    const response = await fetch("https://opentdb.com/api.php?amount=1");
+    const selectedCategory = categorySelect.value;
+    const categoryParam = selectedCategory ? `&category=${selectedCategory}` : "";
+    const response = await fetch(`https://opentdb.com/api.php?amount=1${categoryParam}`);
     const data = await response.json();
-   
-    const question = data.results[0];
-    questionElement.textContent = question.question;
-    answerElement.textContent = ""; 
-    answerButton.dataset.correctAnswer = question.correct_answer;
-}
+  
+    if (data.results && data.results.length > 0) {
+      const question = data.results[0];
+      questionElement.textContent = question.question;
+      answerElement.textContent = "";
+      answerButton.dataset.correctAnswer = question.correct_answer;
+    } else {
+      console.log("API returned no questions.");
+    }
+  };
 
 const getAnswer = () => {
     
